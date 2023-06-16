0c0a9f6a16f651b165414b2c4543c968c7e17d07
inioducode <135998215+inioducode@users.noreply.github.com>
2023-06-16 08:04:04 +0100

    Add files via upload

---
+++ frontend/assets/data/tours.js
@@ -0,0 +1,151 @@
+import tourImg01 from "../tour-images/bahrainwork.jpg";
+import tourImg02 from "../tour-images/czechwork.jpg";
+import tourImg03 from "../tour-images/dubaiwork.jpg";
+import tourImg04 from "../tour-images/italywork.jpg";
+import tourImg05 from "../tour-images/maldiveswork.jpg";
+import tourImg06 from "../tour-images/russiawork.jpg";
+import tourImg07 from "../tour-images/uk.jpg";
+
+const tours = [
+  {
+    id: "01",
+    title: "Westminister Bridge",
+    city: "London",
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+      {
+        name: "jhon doe",
+        rating: 5,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg01,
+    featured: true,
+    type: true,
+  },
+  {
+    id: "02",
+    title: "Bali, Indonesia",
+    city: "Indonesia",
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg02,
+    featured: true,
+  },
+  {
+    id: "03",
+    title: "Snowy Mountains, Thailand",
+    city: "Thailand",
+    distance: 500,
+    address: "Somewhere",
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg03,
+    featured: true,
+  },
+  {
+    id: "04",
+    title: "Beautiful Sunrise, Thailand",
+    city: "Thailand",
+    distance: 500,
+    address: "Somewhere",
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg04,
+    featured: true,
+  },
+  {
+    id: "05",
+    title: "Nusa Pendia Bali, Indonesia",
+    city: "Indonesia",
+    distance: 500,
+    address: "Somewhere",
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg05,
+    featured: false,
+  },
+  {
+    id: "06",
+    title: "Cherry Blossoms Spring",
+    city: "Japan",
+    distance: 500,
+    price: 99,
+    address: "Somewhere",
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg06,
+    featured: false,
+  },
+  {
+    id: "07",
+    title: "Holmen Lofoten",
+    city: "France",
+    distance: 500,
+    price: 99,
+    address: "Somewhere",
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [],
+    avgRating: 4.5,
+    photo: tourImg07,
+    featured: false,
+  },
+  {
+    id: "08",
+    title: "Snowy Mountains, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    address: "Somewhere",
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [],
+    avgRating: 4.5,
+    photo: tourImg03,
+    featured: false,
+  },
+];
+
+export default tours;
